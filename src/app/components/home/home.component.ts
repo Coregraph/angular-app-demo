8ad200a78f2dc303bb19f5c452a3c309b5731ad2
UJAVERIANA\julian.daza <julian.daza@javeriana.edu.co>
2017-07-25 10:51:40 -0500

    Cambio en la plantilla HTML del Componente Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,9 +2,26 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'wo-app-home', //<wo-app-home>
-    template: '<h2>Yo soy el Componente Home</h2>'
+    template: `<h2>Yo soy {{ 'Componente:' + this.nombre }}</h2>
+              <p>Creado el {{ queDiaEsHoy() }}</p>`
 })
 export class HomeComponent{
+    nombre: string = 'Home';
+
     constructor(){
+    }
+
+    queDiaEsHoy(){
+        var today = new Date();
+        var dd:any = today.getDate();
+        var mm:any = today.getMonth()+1;
+        var yyyy:any = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        } 
+        if(mm<10){
+            mm='0'+mm;
+        } 
+        return dd+'/'+mm+'/'+yyyy;
     }
 }
