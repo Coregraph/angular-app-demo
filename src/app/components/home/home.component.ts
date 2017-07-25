ca061a333271583359df13a5011f63c596708e91
UJAVERIANA\julian.daza <julian.daza@javeriana.edu.co>
2017-07-25 10:23:18 -0500

    Primer componente creado.

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'wo-app-home',        //<wo-app-home>
+    template: '<h1>Yo soy el Componente Home</h1>'
+})
+export class HomeComponent{
+    constructor(){
+    }
+}
