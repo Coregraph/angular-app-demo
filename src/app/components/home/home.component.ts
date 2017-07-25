89b57534edb504ab87c6e710b02b2b8a1c89a4fd
UJAVERIANA\julian.daza <julian.daza@javeriana.edu.co>
2017-07-25 10:28:03 -0500

    Registrando el Nuevo Componente en el componente y modulo App

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'wo-app-home',        //<wo-app-home>
-    template: '<h1>Yo soy el Componente Home</h1>'
+    selector: 'wo-app-home', //<wo-app-home>
+    template: '<h2>Yo soy el Componente Home</h2>'
 })
 export class HomeComponent{
     constructor(){
